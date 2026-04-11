92afca1c1c050959b31ba7164b4ba927c8580315
ronak18008 <ronak18008@gmail.com>
2026-04-11 19:15:23 +0530

    ui packages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
 const App = () => {
-  return <div className="text-red-500">Hello</div>
+  return <div className="text-red-500">Hello
+  <button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+  </div>
 }
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import daisyui from 'daisyui'
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+export default {
+  plugins: [daisyui()],
+}
