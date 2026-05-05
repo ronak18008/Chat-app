80fd81f00eda479ced3f44df3d745214b34d5763
ronak18008 <ronak18008@gmail.com>
2026-05-05 15:05:51 +0530

    created  ProfilePage

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-<<<<<<< HEAD
+ HEAD
     baseURL: "http://localhost:4590/api",
-=======
     baseURL: "http://localhost:5173/api",
->>>>>>> 1cd6977db022cba0cb00637bb2aafa1e5639a0cf
     withCredentials: true,
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -53,4 +53,6 @@ export const useAuthStore = create((set) => ({
         toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async(data) => {},
 }))
