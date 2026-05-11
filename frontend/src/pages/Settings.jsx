cff4c31c89bfa68e1ad086f16a39670e3b3d542f
ronak18008 <ronak18008@gmail.com>
2026-05-11 14:50:34 +0530

    Completed setting Page

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -87,9 +87,27 @@ const Settings = () => {
                                                                             </div>
                                                                             </div>
                                                             ))}
-                                                            
-                                                
-                        
+                                                            </div>
+
+                                                            {/* Chat Input */} 
+                                                            <div className="p-4 border-t border-base-300 bg-base-100">
+                                                                <div className="flex gap-2">
+                                                                    <input
+                                                                    type="text"   
+                                                                    className="input input-bordered flex-1 text-sm h-10"
+                                                                    placeholder="Type a message...">
+                                                                        value="This is a preview"
+                                                                        readOnly
+                                                                        />
+                                                                        <button className="btn btn-primary h-10 min-h-0">
+                                                                            <Send size={18} />
+                                                                            </button>
+                                                                            </div>
+                                                                            </div>
+                                                                            </div>
+                                                                            </div>
+                                                                            </div>
+                                                                            </div>
             </div>
             </div>      
     );
