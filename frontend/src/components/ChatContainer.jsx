0f22e2594f35390689336ad2397b01ca5e11dd9f
ronak18008 <ronak18008@gmail.com>
2026-05-11 15:43:05 +0530

    Working on HomePage

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,6 @@
+const ChatContainer = () => {
+    return (
+        <div>ChatContainer</div>
+    )
+}
+export default ChatContainer;
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+
+export const useChatStore = create ((set, get) => ({
+    messages: [],
+    users: [],
+    selectedUser: [],
+    isUserLoading: false,
+    isMessagesLoading: false,
+
+
+    getUsers: async () => {
+        set({ isUserLoading: true});
+        try {
+            const response = await axiosInstance.get("/users");
+            set({ users: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isUserLoading: false });
+        }
+    },
+
+    getMessages: async(userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    }
+    }
+}));
